078bc9cbe3c0fbd3c904b0c51dbae91255a0d073
sg9924 <sg9924@users.noreply.github.com>
2025-08-25 15:32:26 +0530

    Update LED Blink Program

--- stm32f103/led_blink.c
+++ stm32f103/led_blink.c
@@ -1,39 +1,20 @@
-#include<stdint.h>
-#include"stm32f103xx.h"
+#include"stm32f103xx_init.h"
 #include"stm32f103xx_gpio.h"
 
-void wait_ms(uint16_t d)
-{
-	for(int i=0; i<(d*350); i++);
-}
-
-void wait(uint8_t d)
-{
-	wait_ms(d*1000);
-}
 
 int main()
 {
-	//GPIO PC13 Led Pin Settings
+	board_init();
+
 	GPIO_Handle led;
-	
-	//led
-	led.pGPIOx                         = GPIOC;
-	led.GPIOx_PinConfig.PinNo          = GPIO_PIN13;
-	led.GPIOx_PinConfig.PinMode        = GPIO_MODE_OP;
-	led.GPIOx_PinConfig.PinOutputSpeed = GPIO_OP_SPEED_10;
-	led.GPIOx_PinConfig.PinConfigType  = GPIO_GP_OP_PP;
-	
-	//GPIO Init
+	GPIO_Config(&led, GPIOC, GPIO_MODE_OP, GPIO_CONFIG_GP_OP_PP, GPIO_PIN13, GPIO_OP_SPEED_2);
 	GPIO_Init(&led);
-	
-	
-	while(1)
+
+    while(1)
 	{
 		//Toggle PC13 Led Pin
 		GPIO_OpToggle(led.pGPIOx, led.GPIOx_PinConfig.PinNo);
-		wait(1);
+		tim_delay_ms(1000);
+		Serialprint("LED at GPIOC Pin 13 is being toggled!\r\n\0");
 	}
-	
-	return 0;
 }
